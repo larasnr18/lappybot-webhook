bff28c8986444de58cbbb8551fdb98f6668f7f58
larasn6706174044 <larasn@student.telkomuniversity.ac.id>
2020-06-07 18:24:42 +0700

    a

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const restService = express();
 
 restService.use(
   bodyParser.urlencoded({
-    extended: true
+    extended: true,
   })
 );
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
+restService.post("/echo", function (req, res) {
   var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
       ? req.body.queryResult.parameters.echoText
       : "Seems like some problem. Speak again.";
-  
+
   var speechResponse = {
     google: {
       expectUserResponse: true,
@@ -28,28 +28,28 @@ restService.post("/echo", function(req, res) {
         items: [
           {
             simpleResponse: {
-              textToSpeech: speech
-            }
-          }
-        ]
-      }
-    }
+              textToSpeech: speech,
+            },
+          },
+        ],
+      },
+    },
   };
-  
+
   return res.json({
     payload: speechResponse,
     //data: speechResponse,
     fulfillmentText: speech,
     speech: speech,
     displayText: speech,
-    source: "webhook-echo-sample"
+    source: "webhook-echo-sample",
   });
 });
 
-restService.post("/audio", function(req, res) {
+restService.post("/audio", function (req, res) {
   var speech = "";
   switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-    //Speech Synthesis Markup Language 
+    //Speech Synthesis Markup Language
     case "music one":
       speech =
         '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
@@ -135,21 +135,21 @@ restService.post("/audio", function(req, res) {
   return res.json({
     speech: speech,
     displayText: speech,
-    source: "webhook-echo-sample"
+    source: "webhook-echo-sample",
   });
 });
 
-restService.post("/video", function(req, res) {
+restService.post("/video", function (req, res) {
   return res.json({
     speech:
       '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
     displayText:
       '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    source: "webhook-echo-sample"
+    source: "webhook-echo-sample",
   });
 });
 
-restService.post("/slack-test", function(req, res) {
+restService.post("/slack-test", function (req, res) {
   var slack_message = {
     text: "Details of JIRA board for Browse and Commerce",
     attachments: [
@@ -162,17 +162,17 @@ restService.post("/slack-test", function(req, res) {
           {
             title: "Epic Count",
             value: "50",
-            short: "false"
+            short: "false",
           },
           {
             title: "Story Count",
             value: "40",
-            short: "false"
-          }
+            short: "false",
+          },
         ],
 
         thumb_url:
-          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
+          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png",
       },
       {
         title: "Story status count",
@@ -183,37 +183,37 @@ restService.post("/slack-test", function(req, res) {
           {
             title: "Not started",
             value: "50",
-            short: "false"
+            short: "false",
           },
           {
             title: "Development",
             value: "40",
-            short: "false"
+            short: "false",
           },
           {
             title: "Development",
             value: "40",
-            short: "false"
+            short: "false",
           },
           {
             title: "Development",
             value: "40",
-            short: "false"
-          }
-        ]
-      }
-    ]
+            short: "false",
+          },
+        ],
+      },
+    ],
   };
   return res.json({
     speech: "speech",
     displayText: "speech",
     source: "webhook-echo-sample",
     data: {
-      slack: slack_message
-    }
+      slack: slack_message,
+    },
   });
 });
 
-restService.listen(process.env.PORT || 8000, function() {
+restService.listen(process.env.PORT || 8000, function () {
   console.log("Server up and listening");
 });
---
+++ try.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+async function getNumberOfFollowers() {
+  let res = await axios.get("https://api.github.com/users/janbodnar");
+
+  let nOfFollowers = res.data.followers;
+  let location = res.data.location;
+
+  const data = { nOfFollowers, location };
+  console.log(`# of followers: ${nOfFollowers}`);
+  console.log(`Location: ${location}`);
+
+  return data;
+}
+
+getNumberOfFollowers();
+
+module.exports = { getNumberOfFollowers };
